b96ce85600bc5d90ec6a5a026fbb27f8727a8223
Sunday <sunday@Sundays-MacBook-Pro.local>
2022-12-20 14:09:45 +1100

    fourth commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   const [showMore, setShowMore] = useState(false);
   
   return (
-    <div className="grid justify-items-center content-start pr-80">
+    <div className="grid justify-items-center content-start">
       <h1 class="field">field</h1>
       <div className="h-4"></div>
       <h2 className="w-60">a calm technology</h2>
