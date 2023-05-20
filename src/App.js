271c62922f1a3e8497958bd2fd53ade3fb824cbf
Sunday <sunday@sundays-mbp.home>
2023-05-20 12:49:24 +1000

    v14

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Deck from './Deck';
 
 const HeadingWithSpacing = ({ children }) => (
   <>
-    <h2 className="w-full px-4 mx-2 lg:w-1/2 lg:px-0 lg:mx-0 2xl:w-[708px] text-2xl">{children}</h2>
+    <h2 className="w-full px-6 py-4 mx-6 text-lg sm:w-full sm:px-4 sm:mx-2 lg:w-1/2 lg:px-0 lg:mx-0 2xl:w-[708px] text-2xl">{children}</h2>
     <div className="h-8"></div>
   </>
 );
@@ -12,12 +12,12 @@ const HeadingWithSpacing = ({ children }) => (
 const MoreSection = ({ showMore }) => (
   <>
     <div className="h-8"></div>
-    <p className="text-2xl">
+    <p className="text-lg sm:text-2xl">
       create a blog forever owned by you via blockchain technology; available
       online and offline
     </p>
     <div className="h-8"></div>
-    <p className="text-2xl">
+    <p className="text-lg sm:text-2xl">
       a beautiful non-biased place to express, share and connect all online and personal content with minimal restrictions, akin to a personal diary or archive
     </p>
     <div className="h-8"></div>
@@ -29,7 +29,7 @@ function HomePage() {
 
   return (
     <div className="grid justify-items-center content-start">
-      <div className="h-60"></div>
+      <div className="h-24 sm:h-60"></div>
       <HeadingWithSpacing>
         this is a blogging platform where you own your content forever, online
         and offline
@@ -37,11 +37,11 @@ function HomePage() {
       <HeadingWithSpacing>home for the pages of your mind</HeadingWithSpacing>
       <HeadingWithSpacing>no ads</HeadingWithSpacing>
       <HeadingWithSpacing>no censorship</HeadingWithSpacing>
-      <div className="w-full px-4 mx-2 lg:w-1/2 lg:px-0 lg:mx-0 2xl:w-[708px]">
+      <div className="w-full px-6 py-4 mx-6 text-lg sm:w-full sm:px-4 sm:mx-2 lg:w-1/2 lg:px-0 lg:mx-0 2xl:w-[708px]">
         <button
           id="moreBtn"
           onClick={() => setShowMore(!showMore)}
-          className="font-bold text-2xl inline-block"
+          className="font-bold text-lg sm:text-2xl inline-block"
         >
           {showMore === true ? "- more" : "+  more"}
         </button>
@@ -55,7 +55,7 @@ function HomePage() {
 export default function App() {
   return (
     <>
-      <header className="fixed top-0 left-0 p-4 text-sm font-bold">
+      <header className="fixed top-0 left-0 px-6 py-4 text-xs font-bold sm:p-4 sm:text-sm">
         field
       </header>
       <Routes>
